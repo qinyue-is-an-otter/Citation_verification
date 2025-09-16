b0e19af89c2a851b59a818ac184982c16371343d
birdie0111 <celineliu0111@gmail.com>
2025-09-16 06:04:54 +0200

    pmc key and mail anonymized

--- Pipeline/Tools/functions_abstract.py
+++ Pipeline/Tools/functions_abstract.py
@@ -18,8 +18,8 @@ logger.addHandler(file_handler)
 url = "https://www.ncbi.nlm.nih.gov/pmc/utils/idconv/v1.0/"
 
 
-key = ""#"fd895b77ece1cd582d9d2a40cc6d23f88008" # NCBI key
-mail = ""#'recheinje@gmail.com'
+key = ""
+mail = ""
 
 
 USER_AGENT = f"YourAppName/1.0 (mailto:{mail})"
